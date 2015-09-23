a58be38e720e7ebdd8fb6fa953af1b47dbec663e
hanquez <hanquez@a13p18.fil.univ-lille1.fr>
2015-09-23 17:23:48 +0200

    Ajout salle Dragon (a faire)

---
+++ src/dungeon/room/DragonRoom.java
@@ -0,0 +1,19 @@
+package dungeon.room;
+
+import dungeon.entity.Player;
+
+public class DragonRoom extends Room {
+
+	@Override
+	public String getExtendedDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onPlayerEnter(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
